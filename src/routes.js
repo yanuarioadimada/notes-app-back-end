45f6b80929e77cc13ff1c9c7b2723dfa9e793712
yanuarioadimada <yanuarioadimada@gmail.com>
2024-11-06 00:53:34 +0700

    changes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { addNoteHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
 
 const routes = [
   {
@@ -6,6 +6,16 @@ const routes = [
     path: '/notes',
     handler: addNoteHandler,
   },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  }
 ];
 
 module.exports = routes;
